66d1d0ada9a89e74557fa4f3da665a1ca811ded8
Shuopeng Zhou <shuopeng.zhou@oracle.com>
2020-12-13 12:22:22 -0500

    Update code

--- name_NER.py
+++ name_NER.py
@@ -6,15 +6,13 @@ from nltk.tag.stanford import StanfordNERTagger
 jar = './stanford-ner-2018-10-16/stanford-ner.jar'
 model = './stanford-ner-2018-10-16/classifiers/4_class_2020_11.gz'
 
-simpleModel = './stanford-ner-2018-10-16/classifiers/english.all.3class.distsim.crf.ser.gz'
 
 # Prepare NER tagger with english model
 ner_tagger = StanfordNERTagger(model, jar, encoding='utf8')
-# ner_tagger_2 = StanfordNERTagger(simpleModel, jar, encoding='utf8')
 
-for i in range(1524, 6524):
+for i in range(1):
     indexOfFile = i
-    with open('data/compiled_bios/' + str(i) + '.txt', 'r') as file:
+    with open('data/compiled_bios/' + str(93) + '.txt', 'r') as file:
         data = file.read().replace('\n', '')
         isOutOfIndex = False
         dic = {}
@@ -25,7 +23,7 @@ for i in range(1524, 6524):
         # print(results)
         # Run NER tagger on words
         for i in range(len(results)):
-            if i != 0 and i != len(results) - 1:
+            if i != 0 and i != len(results) - 1 and isOutOfIndex == False:
                 formerWord = results[i - 1]
                 latterWord = results[i + 1]
                 currentWord = results[i]            
@@ -38,23 +36,21 @@ for i in range(1524, 6524):
                         index = index - 1
                         # wordCount = wordCount + 1
                         if index == 0: 
-                            # isOutOfIndex = True
-                            # res = fullName
+                            isOutOfIndex = True
+                            res = fullName
                             break
                         formerWord = results[index - 1]
                     
-                        # Remove parenthese
-                        # fullName = re.sub(r" ?\([^)]+\)", "", fullName)
-                        fullName = fullName.replace(")", "")
-                        fullName = fullName.replace("(", "")
-                        fullName = fullName.replace("]", "")
-                        fullName = fullName.replace("[", "")
-                        fullName = fullName.replace("\\", "")
-                        fullName = fullName.replace("\(\(\(", ">>")
-                        fullName = fullName.strip()
-                        dic[fullName] = dic.get(fullName, 0) + 1
+                    fullName = fullName.replace(")", "")
+                    fullName = fullName.replace("(", "")
+                    fullName = fullName.replace("]", "")
+                    fullName = fullName.replace("[", "")
+                    fullName = fullName.replace("\\", "")
+                    fullName = fullName.replace("\(\(\(", ">>")
+                    fullName = fullName.strip()
+                    dic[fullName] = dic.get(fullName, 0) + 1
         if (isOutOfIndex == True):
-            f = open("res.txt", "a")
+            f = open("names.txt", "a")
             f.write(res + "\n")
             f.close()
         if (isOutOfIndex == False):
@@ -80,16 +76,17 @@ for i in range(1524, 6524):
                 if item[1] == highestFrequency:
                     if len(item[0]) > len(res):
                         res = item[0]
-            # print(dic)
+            print(dic)
             # print(res)
             # print(indexOfFile)
             # Cleanup phase
             if (res != ""):
                 res = res.replace('Professor', '')
-            
+                if ('Goodwin' in res):
+                    res = ""
             # print(cleanedUpRes)
             
-            f = open("res.txt", "a")
+            f = open("names.txt", "a")
             f.write(res + "\n")
             f.close()
     file.close()
